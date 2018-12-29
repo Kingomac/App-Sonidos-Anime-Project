d4adb85eb8bf42bdd39a9f69f9f48183ba1500c6
Kingomac <kingomacyt@gmail.com>
2018-12-29 14:45:31 +0100

    Compatir y reproducir en Boku No Hero
    Y optimizaciones para los anteriores

--- Assets/Scripts/Compartir/ShareController.cs
+++ Assets/Scripts/Compartir/ShareController.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System;
 using UnityEngine;
-using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class ShareController : MonoBehaviour
 {
     public AudioController audioController;
+    public Button parentButton;
     public string[] Links;
 
+    public void Start()
+    {
+        SetParentButton(false);
+    }
+    private void OnDisable()
+    {
+        SetParentButton(true);
+    }
     public void SetLinks(string[] a)
     {
         Links = a;
@@ -19,5 +28,10 @@ public class ShareController : MonoBehaviour
     public string GetLink(int n)
     {
         return Links[n];
+    }
+    public void SetParentButton(bool active)
+    {
+        parentButton = audioController.gameObject.GetComponent<Button>();
+        parentButton.enabled = active;
     }
 }
