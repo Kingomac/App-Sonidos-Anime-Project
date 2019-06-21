0aab86f673ad19994115257204ee8b04db971812
Kingomac <kingomacyt@gmail.com>
2019-06-21 13:43:00 +0200

    Empieza la 2.0
    Primeros cambios para la versión 2.0

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -3,64 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioController : MonoBehaviour {
-    public AudioSource[] audios;
-    public int _soundnum;
-    public string listLink;
+    public AudioClip[] audios;
+    private AudioSource audioSource;
 	// Use this for initialization
 	void Start () {
-        audios = GetComponents<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
     /// <summary>
     /// Reproducir uno de los AudioSource aleatorios
     /// </summary>
     public void Play()
     {
-        try
-        {
-            FindObjectOfType<AudioController>().StopAll();
-        }
-        catch
-        {
-        }
-        try
-        {
-            FindObjectOfType<StopVideos>().PauseAll();
-        }
-        catch
-        {
-        }
-        bool isPlaying = false;
-        for(int i = 0; i < audios.Length; i++)
-        {
-            if (audios[i].isPlaying)
-            {
-                isPlaying = true;
-                break;
-            }
-        }
-        _soundnum = Random.Range(0, audios.Length);
-        if (!isPlaying) audios[_soundnum].Play();
-        Debug.Log(_soundnum);
-    }
-    /// <summary>
-    /// Detiene todos los audios que estén sonando, NO funciona con vídeos.
-    /// </summary>
-    public void StopAll()
-    {
-        GameObject[] objects = GameObject.FindGameObjectsWithTag("SoundButton");
-        AudioSource[] sr;
-        for (int i = 0; i < objects.Length; i++)
-        {
-            sr = objects[i].GetComponents<AudioSource>();
-            for (int z = 0; z < sr.Length; z++)
-            {
-                if (sr[z].isPlaying)
-                {
-                    sr[z].Stop();
-                    break;
-                }
-            }
-        }
+        audioSource.clip = audios[Random.Range(0, audios.Length)];
+        audioSource.Play();
     }
     public void FindAndStopAll()
     {
@@ -76,20 +31,8 @@ public class AudioController : MonoBehaviour {
     /// <param name="audioNum"></param>
     public void SelectAndPlay(int audioNum)
     {
-        try
-        {
-            FindObjectOfType<AudioController>().StopAll();
-        }
-        catch
-        {
-        }
-        try
-        {
-            FindObjectOfType<StopVideos>().PauseAll();
-        }
-        catch
-        {
-        }
-        audios[audioNum].Play();
+        FindAndStopAll();
+        audioSource.clip = audios[audioNum];
+        audioSource.Play();
     }
 }
--- Assets/Scripts/PantallaDeCarga.cs
+++ Assets/Scripts/PantallaDeCarga.cs
@@ -1,63 +1,34 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PantallaDeCarga : MonoBehaviour
 {
-    public static PantallaDeCarga Instancia { get; private set; }
-    public Image imageDeCarga;
-    [Range(0.01f, 0.5f)]
-    public float velocidad = 0.5f;
-    void Awake()
+    public GameObject pantalla;
+    // Start is called before the first frame update
+    void Start()
     {
-        DefinirSingleton();
+        
     }
-    private void DefinirSingleton()
+
+    // Update is called once per frame
+    void Update()
     {
-        if (Instancia == null)
-        {
-            Instancia = this;
-            DontDestroyOnLoad(this);
-            imageDeCarga.gameObject.SetActive(false);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+      
     }
-    public void CargarEscena(string nombreEscena)
+    IEnumerator LoadScene(string name)
     {
-        StartCoroutine(MostrarPantallaDeCarga(nombreEscena));
+        pantalla.SetActive(true);
+        yield return new WaitForSeconds(2);
+        AsyncOperation a = SceneManager.LoadSceneAsync(name,LoadSceneMode.Additive);
+        while (!a.isDone) yield return null;
+        pantalla.SetActive(false);
+        //Scene s = SceneManager.GetSceneByName(name);
+        //SceneManager.SetActiveScene(s);
     }
-    private IEnumerator MostrarPantallaDeCarga(string nombreEscena)
+    public void ChangeScene(string name)
     {
-        imageDeCarga.gameObject.SetActive(true);
-        Color c = imageDeCarga.color;
-        c.a = 0.0f;
-        //Mientras no esté totalmente visible va aumentando su visibilidad
-        while (c.a < 1)
-        {
-            imageDeCarga.color = c;
-            c.a += velocidad;
-            yield return null;
-        }
-        c.a = 1;
-        imageDeCarga.color = c;
-        //Carga la escena
-        SceneManager.LoadSceneAsync(nombreEscena);
-        //Espera a que haya cargado la nueva escena
-        while (!nombreEscena.Equals(SceneManager.GetActiveScene().name))
-        {
-            yield return null;
-        }
-        //Mientras la imagen de carga siga visible va desvaneciéndola
-        while (c.a > 0)
-        {
-            imageDeCarga.color = c;
-            c.a -= velocidad;
-            yield return null;
-        }
-        imageDeCarga.gameObject.SetActive(false);
+        StartCoroutine(LoadScene(name));
     }
 }
