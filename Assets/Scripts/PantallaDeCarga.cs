cc946461f270f339db043e426997630c2cb64fe2
Kingomac <kingomacyt@gmail.com>
2019-06-22 18:05:54 +0200

    Fade In de la pantalla de carga

--- Assets/Scripts/PantallaDeCarga.cs
+++ Assets/Scripts/PantallaDeCarga.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PantallaDeCarga : MonoBehaviour
 {
     public GameObject pantalla;
+    public Image img;
+    public float a;
     // Start is called before the first frame update
     void Start()
     {
-        
+        img = pantalla.GetComponentInChildren<Image>();
     }
 
     // Update is called once per frame
@@ -20,12 +23,13 @@ public class PantallaDeCarga : MonoBehaviour
     IEnumerator LoadScene(string name)
     {
         pantalla.SetActive(true);
+        img.CrossFadeAlpha(255, 2, false);
         yield return new WaitForSeconds(2);
         AsyncOperation a = SceneManager.LoadSceneAsync(name,LoadSceneMode.Additive);
         while (!a.isDone) yield return null;
         pantalla.SetActive(false);
-        //Scene s = SceneManager.GetSceneByName(name);
-        //SceneManager.SetActiveScene(s);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(name));
+        img.color = new Color(img.color.r, img.color.g, img.color.b, 0);
     }
     public void ChangeScene(string name)
     {
